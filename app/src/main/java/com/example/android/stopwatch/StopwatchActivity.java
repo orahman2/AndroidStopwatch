13a1c07e49d73be3f4b599e827fff39e947dcbe8
Omar <omarrahman96@gmail.com>
2016-08-19 19:23:47 +0400

    built basic framework

---
+++ app/src/main/java/com/example/android/stopwatch/StopwatchActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.stopwatch;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
